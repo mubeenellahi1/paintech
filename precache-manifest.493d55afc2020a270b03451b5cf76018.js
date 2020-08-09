5939598df905c44090a890db382f307a0ed1297c
Mubeen Ellahi <mubeenellahi@Mubeens-MacBook-Pro.local>
2020-08-09 16:31:40 +0500

    Updates

---
+++ precache-manifest.493d55afc2020a270b03451b5cf76018.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "075b40a706f6415f6184ac2b96fbddb3",
+    "url": "/paintedch/index.html"
+  },
+  {
+    "revision": "01ef0f93fbca1a98b100",
+    "url": "/paintedch/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "06c0b5bb5d3d30a84c9f",
+    "url": "/paintedch/static/css/main.086b4f39.chunk.css"
+  },
+  {
+    "revision": "01ef0f93fbca1a98b100",
+    "url": "/paintedch/static/js/2.b7199b5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/paintedch/static/js/2.b7199b5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c0b5bb5d3d30a84c9f",
+    "url": "/paintedch/static/js/main.ad2d9802.chunk.js"
+  },
+  {
+    "revision": "4417ca7f34972542903c",
+    "url": "/paintedch/static/js/runtime-main.c0fb4e36.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/paintedch/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "07d63b4d0376fca3bbedc274d9dd9233",
+    "url": "/paintedch/static/media/RobotoSlab-Light.07d63b4d.ttf"
+  },
+  {
+    "revision": "185372e38bd5726eaaf518174da5d055",
+    "url": "/paintedch/static/media/bitmap-a-a-1@1x.185372e3.png"
+  },
+  {
+    "revision": "e7743397535ba0737d3a84d4dd8f4672",
+    "url": "/paintedch/static/media/bitmap-a-a-2@1x.e7743397.png"
+  },
+  {
+    "revision": "5cd832b43176b43248101c5f067ba5ea",
+    "url": "/paintedch/static/media/bitmap-a-a-3@1x.5cd832b4.png"
+  },
+  {
+    "revision": "002261a82ff9abc12cdb08808ff7d628",
+    "url": "/paintedch/static/media/bitmap-l-u-1@1x.002261a8.png"
+  },
+  {
+    "revision": "6b8d05c6dc4630ac5f03bc22ff7833c2",
+    "url": "/paintedch/static/media/bitmap-l-u-2@1x.6b8d05c6.png"
+  },
+  {
+    "revision": "5ac8b91530c3a7fe201eb33f34d9232d",
+    "url": "/paintedch/static/media/bitmap-l-u-3@1x.5ac8b915.png"
+  },
+  {
+    "revision": "0c34f2f23f08020db88c28c847736816",
+    "url": "/paintedch/static/media/bitmap-m-a-1@1x.0c34f2f2.jpg"
+  },
+  {
+    "revision": "ba215fb237553171c4a844132b95ebc3",
+    "url": "/paintedch/static/media/bitmap-m-a-2@1x.ba215fb2.png"
+  },
+  {
+    "revision": "e5bc553a85356bebe6b404338c7fdffd",
+    "url": "/paintedch/static/media/bitmap-m-a-3@1x.e5bc553a.jpg"
+  },
+  {
+    "revision": "62c49f7903c22afc139a8c34df3e5333",
+    "url": "/paintedch/static/media/bitmap-media-1@1x.62c49f79.png"
+  },
+  {
+    "revision": "6f377025ecc90544baaa9e494782c6cc",
+    "url": "/paintedch/static/media/bitmap-media-2@1x.6f377025.png"
+  },
+  {
+    "revision": "41f75e3052cc0df723def9037a48f117",
+    "url": "/paintedch/static/media/bitmap-media-3@1x.41f75e30.png"
+  },
+  {
+    "revision": "e0978ca8cd08b8bf20212a2d8abec609",
+    "url": "/paintedch/static/media/bitmap-p-1@1x.e0978ca8.png"
+  },
+  {
+    "revision": "8908fde6924676eb7a7e4877a1a50d44",
+    "url": "/paintedch/static/media/bitmap-p-2@1x.8908fde6.png"
+  },
+  {
+    "revision": "755f43381634666d79ccabfb7701b583",
+    "url": "/paintedch/static/media/bitmap-p-3@1x.755f4338.png"
+  },
+  {
+    "revision": "9cd0af3448129a368ccad626485d03d2",
+    "url": "/paintedch/static/media/bitmap-p-p-1@1x.9cd0af34.png"
+  },
+  {
+    "revision": "11f020f344b2c3a206e2b4494351efee",
+    "url": "/paintedch/static/media/bitmap-p-p-2@1x.11f020f3.png"
+  },
+  {
+    "revision": "3cfc307c07cbcf3e73b3ca7898fe4c08",
+    "url": "/paintedch/static/media/bitmap-p-p-3@1x.3cfc307c.png"
+  },
+  {
+    "revision": "114ad17fe9b3af3c7775de8b6730aacc",
+    "url": "/paintedch/static/media/bitmap_b_p_1@1x.114ad17f.png"
+  },
+  {
+    "revision": "1b2a38df3ea633501008569e255a5677",
+    "url": "/paintedch/static/media/bitmap_b_p_2@1x.1b2a38df.png"
+  },
+  {
+    "revision": "1df4bda0afe5283f92847ad9490d80a7",
+    "url": "/paintedch/static/media/bitmap_b_s@1x.1df4bda0.png"
+  },
+  {
+    "revision": "377afd70dd16d1d164892618f2657ffb",
+    "url": "/paintedch/static/media/bitmap_s_e@1x.377afd70.png"
+  },
+  {
+    "revision": "c3353e60647d45969ff002ea760a90b3",
+    "url": "/paintedch/static/media/brush-1.c3353e60.png"
+  },
+  {
+    "revision": "2d2115633c963048c9a22b47c1537fb0",
+    "url": "/paintedch/static/media/brush-10@1x.2d211563.png"
+  },
+  {
+    "revision": "8894b6aa88ea57a353307393a81beed7",
+    "url": "/paintedch/static/media/brush-14@1x.8894b6aa.png"
+  },
+  {
+    "revision": "a59d1ba398e732d8456ab9e690bf88f9",
+    "url": "/paintedch/static/media/brush-1@1x.a59d1ba3.png"
+  },
+  {
+    "revision": "c9e6763abcf012273c538f2b701a7969",
+    "url": "/paintedch/static/media/brush-2.c9e6763a.png"
+  },
+  {
+    "revision": "461d5dc165ccb6225c5bf0d848d81812",
+    "url": "/paintedch/static/media/brush-2@1x.461d5dc1.png"
+  },
+  {
+    "revision": "d9dbd9d3f093d802c62847d957686e80",
+    "url": "/paintedch/static/media/brush-3@1x.d9dbd9d3.png"
+  },
+  {
+    "revision": "1581843d2329453092314f637b8de58d",
+    "url": "/paintedch/static/media/brush-4@1x.1581843d.png"
+  },
+  {
+    "revision": "9a86b77772d3afea6b364124f57891ee",
+    "url": "/paintedch/static/media/brush-5@1x.9a86b777.png"
+  },
+  {
+    "revision": "3d2f68ab4d1f0e9101adff024e16316c",
+    "url": "/paintedch/static/media/brush-6@1x.3d2f68ab.png"
+  },
+  {
+    "revision": "de85cdf3fc70c7e3e80472e73b60db98",
+    "url": "/paintedch/static/media/brush-7@1x.de85cdf3.png"
+  },
+  {
+    "revision": "8894b6aa88ea57a353307393a81beed7",
+    "url": "/paintedch/static/media/brush-8@1x.8894b6aa.png"
+  },
+  {
+    "revision": "6b149efa89db6390bc9cb29536fce8d2",
+    "url": "/paintedch/static/media/brush-9@1x.6b149efa.png"
+  },
+  {
+    "revision": "824425156bfa5d569072f94f535d8c85",
+    "url": "/paintedch/static/media/brush-media-1@2x.82442515.png"
+  },
+  {
+    "revision": "4889f49422195a62387b7c92e3177d2f",
+    "url": "/paintedch/static/media/brush-media-2@2x.4889f494.png"
+  },
+  {
+    "revision": "b503e9d5d4409defa0a899c6af9e56bc",
+    "url": "/paintedch/static/media/brush-media-3@2x.b503e9d5.png"
+  },
+  {
+    "revision": "4b8412578233707224f1bbb0160a9a5b",
+    "url": "/paintedch/static/media/brush-media-4@2x.4b841257.png"
+  },
+  {
+    "revision": "82c9e33e6a2441ff5765910e5ae8cd7d",
+    "url": "/paintedch/static/media/brush-media-5@2x.82c9e33e.png"
+  },
+  {
+    "revision": "8c318e387300658e6c729b7abe2e8c11",
+    "url": "/paintedch/static/media/canvas-studio-media@1x.8c318e38.png"
+  },
+  {
+    "revision": "788a784d730bd671b3866d7d620b68f5",
+    "url": "/paintedch/static/media/canvas-studio@1x.788a784d.png"
+  },
+  {
+    "revision": "86ec4b10d4f2ec1bed272bf441484945",
+    "url": "/paintedch/static/media/drop-10@2x.86ec4b10.png"
+  },
+  {
+    "revision": "b301e8d86db5802bcfc7b17ae1d1d8f7",
+    "url": "/paintedch/static/media/drop-14@1x.b301e8d8.png"
+  },
+  {
+    "revision": "454a575c13eaf4d4a7768f39f003bae8",
+    "url": "/paintedch/static/media/drop-17-1@1x.454a575c.png"
+  },
+  {
+    "revision": "f54c839280394da615d8ff7416198727",
+    "url": "/paintedch/static/media/drop-17@1x.f54c8392.png"
+  },
+  {
+    "revision": "2caf19c5c696993ca0a0371249476ccb",
+    "url": "/paintedch/static/media/drop-1@1x.2caf19c5.png"
+  },
+  {
+    "revision": "7423fbfd1342d9fded7835b976ebc6e0",
+    "url": "/paintedch/static/media/drop-20@2x.7423fbfd.png"
+  },
+  {
+    "revision": "cf7012756d2277595ddb047007c7744b",
+    "url": "/paintedch/static/media/drop-21-2@2x.cf701275.png"
+  },
+  {
+    "revision": "d7fbff940044a6535549d476581f7c06",
+    "url": "/paintedch/static/media/drop-23-2@1x.d7fbff94.png"
+  },
+  {
+    "revision": "7e3633b6d7ebca7f8c788ed4e1fa46e8",
+    "url": "/paintedch/static/media/drop-2@1x.7e3633b6.png"
+  },
+  {
+    "revision": "c749ee5aada2ad9fca6ba3216500dc25",
+    "url": "/paintedch/static/media/drop-5-2@1x.c749ee5a.png"
+  },
+  {
+    "revision": "1ebf6b563185c62c310a8257f7601429",
+    "url": "/paintedch/static/media/drop-5@1x.1ebf6b56.png"
+  },
+  {
+    "revision": "242efd4d3db47b054de3440180bfaeed",
+    "url": "/paintedch/static/media/drop-6@1x.242efd4d.png"
+  },
+  {
+    "revision": "bfb14797d2b2440dcb7b4c812388a640",
+    "url": "/paintedch/static/media/drop-7-3@1x.bfb14797.png"
+  },
+  {
+    "revision": "e84904a485d29a15ba0ae26c3f41f85c",
+    "url": "/paintedch/static/media/drop-7-copy@1x.e84904a4.png"
+  },
+  {
+    "revision": "feb373d30d486fa02dc62f3107c493a0",
+    "url": "/paintedch/static/media/drop-media-1@1x.feb373d3.png"
+  },
+  {
+    "revision": "9987a4f18e7b70bdd084fb57369f18bf",
+    "url": "/paintedch/static/media/drop-media-6@1x.9987a4f1.png"
+  },
+  {
+    "revision": "c3ae752f21646dbf3abd0eb016e7d602",
+    "url": "/paintedch/static/media/drop-media-8@1x.c3ae752f.png"
+  },
+  {
+    "revision": "31a1c2f1d6e1e44e5d2ebb1ec4a378fa",
+    "url": "/paintedch/static/media/drop-media-9@1x.31a1c2f1.png"
+  },
+  {
+    "revision": "a7318ed210a1c593b2a760deb2eca25f",
+    "url": "/paintedch/static/media/home-image@1x.a7318ed2.png"
+  },
+  {
+    "revision": "88bf5041a8940efbabdc3349cca715c8",
+    "url": "/paintedch/static/media/home-image@2x.88bf5041.png"
+  },
+  {
+    "revision": "c1d9c55664cd1f9fecb565383b5cca02",
+    "url": "/paintedch/static/media/image@1x.c1d9c556.png"
+  },
+  {
+    "revision": "9b73995ffa7868590aae39bad3a601ad",
+    "url": "/paintedch/static/media/oval-2-copy-7@1x.9b73995f.png"
+  },
+  {
+    "revision": "2241e310a739af72f44be311611e79f1",
+    "url": "/paintedch/static/media/sofa@1x.2241e310.png"
+  }
+]);
